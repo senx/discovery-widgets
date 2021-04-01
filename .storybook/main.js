4ba0e6e02513e8e9d12613ecbc8becb88c422d26
xavier <marin.xavier@gmail.com>
2021-04-01 17:28:50 +0200

    deps update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,11 @@
-const path = require('path')
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     '@storybook/addon-actions'
-  ],
- /* webpackFinal: async (config, { configType }) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "lib": path.resolve(__dirname, "./www/build")
-    };
-
-    // Return the altered config
-    return config;
-  }*/
+  ]
 }
