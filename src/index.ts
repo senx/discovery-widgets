e094706a6eaadbe0b13e4ecc8b303d0da928087c
xavier <marin.xavier@gmail.com>
2021-06-23 17:12:50 +0200

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 (window as any).global = window;
-export { Components, JSX } from './components';
-export { Param } from './model/param'
-export { GTS } from './model/GTS'
-export { Tile } from './model/tile'
-export { ChartType } from './model/types'
+export {Components, JSX} from './components';
+export {Param} from './model/param'
+export {GTS} from './model/GTS'
+export {Tile} from './model/tile'
+export {ChartType} from './model/types'
 export {ChartBounds} from "./model/chartBounds";
 export {Dashboard} from "./model/dashboard";
 export {DataModel} from "./model/dataModel";
 export {DiscoveryEvent} from "./model/discoveryEvent";
 export {Label} from "./model/label";
 export {ColorLib} from "./utils/color-lib";
+export {MapLib} from "./utils/map-lib";
