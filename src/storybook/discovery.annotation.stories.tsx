76f5d3617e4bf0784cf1eac0cdcf280c3f59c072
xavier <marin.xavier@gmail.com>
2022-01-04 14:07:13 +0100

    copyright update

--- src/storybook/discovery.annotation.stories.tsx
+++ src/storybook/discovery.annotation.stories.tsx
@@ -1,7 +1,5 @@
-// noinspection JSUnusedGlobalSymbols
-
 /*
- *   Copyright 2021  SenX S.A.S.
+ *   Copyright 2022  SenX S.A.S.
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -15,6 +13,8 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
+
+// noinspection JSUnusedGlobalSymbols
 
 import tile, {Usage} from './discovery.tile.stories';
 import {Param} from "../model/param";
--- src/storybook/discovery.chartSync.stories.tsx
+++ src/storybook/discovery.chartSync.stories.tsx
@@ -1,7 +1,5 @@
-// noinspection JSUnusedGlobalSymbols
-
 /*
- *   Copyright 2021  SenX S.A.S.
+ *   Copyright 2022  SenX S.A.S.
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -15,6 +13,8 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
+
+// noinspection JSUnusedGlobalSymbols
 
 import {Param} from "../model/param";
 import {action} from '@storybook/addon-actions';
