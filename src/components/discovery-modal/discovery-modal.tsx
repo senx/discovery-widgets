4c232e24101a93dbbcc8a8f4634ecdb82dad367c
Xavier Marin <xavier.marin@senx.io>
2023-04-20 09:04:52 +0200

    Fix modal display order and content parsing.
    Signed-off-by: Xavier Marin <xavier.marin@senx.io>

--- src/components/discovery-modal/discovery-modal.tsx
+++ src/components/discovery-modal/discovery-modal.tsx
@@ -21,6 +21,7 @@ import {Utils} from '../../utils/utils';
 import {Param} from '../../model/param';
 import {Logger} from '../../utils/logger';
 import {JsonLib} from '../../utils/jsonLib';
+import {GTSLib} from '../../utils/gts.lib';
 
 @Component({
   tag: 'discovery-modal',
@@ -58,10 +59,7 @@ export class DiscoveryModalComponent {
     if (!!this.data && typeof this.data === 'string') {
       this.data = new JsonLib().parse(this.data as string);
     }
-    this.LOG?.debug(['dataUpdate'], {
-      data: this.data,
-      newValue, oldValue
-    });
+    this.LOG?.debug(['dataUpdate'], {data: this.data, newValue, oldValue});
     this.parseData();
   }
 
@@ -77,6 +75,9 @@ export class DiscoveryModalComponent {
   }
 
   private parseData() {
+    if(GTSLib.isArray(this.data)) {
+      this.data = this.data[0];
+    }
     if (this.data) {
       if ((this.data as any).macro || (this.data as any).data) {
         this.dashboard = undefined;
