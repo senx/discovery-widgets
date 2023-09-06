8cf6fc8b67e83188b4d36e3e62c95e10cc384860
Xavier Marin <xavier.marin@senx.io>
2023-09-06 14:53:42 +0200

    [map] Fix 'oceans' tile server url
    Signed-off-by: Xavier Marin <xavier.marin@senx.io>

--- src/utils/map-lib.ts
+++ src/utils/map-lib.ts
@@ -108,7 +108,7 @@ export class MapLib {
  IGP, UPR-EGP, and the GIS User Community`
     },
     OCEANS: {
-      link: 'https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}',
+      link: 'https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}',
       attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
     },
     GRAY: {
