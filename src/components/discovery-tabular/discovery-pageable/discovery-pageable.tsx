db107ab2f1f54d6b3e91a9f61cfcf3ef4f96e913
Xavier Marin <xavier.marin@senx.io>
2024-10-31 10:09:08 +0100

    [tabular] Fix infinite loop
    Signed-off-by: Xavier Marin <xavier.marin@senx.io>

--- src/components/discovery-tabular/discovery-pageable/discovery-pageable.tsx
+++ src/components/discovery-tabular/discovery-pageable/discovery-pageable.tsx
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2022-2023  SenX S.A.S.
+ *   Copyright 2022-2024  SenX S.A.S.
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -15,12 +15,12 @@
  */
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars,max-classes-per-file
-import {Component, Event, EventEmitter, h, Prop, Watch} from '@stencil/core';
-import {Logger} from '../../../utils/logger';
-import {Param} from '../../../model/param';
-import {GTSLib} from '../../../utils/gts.lib';
-import {Utils} from '../../../utils/utils';
-import {Dataset} from '../../../model/types';
+import { Component, Event, EventEmitter, h, Prop, State, Watch } from '@stencil/core';
+import { Logger } from '../../../utils/logger';
+import { Param } from '../../../model/param';
+import { GTSLib } from '../../../utils/gts.lib';
+import { Utils } from '../../../utils/utils';
+import { Dataset } from '../../../model/types';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import duration from 'dayjs/plugin/duration';
@@ -32,7 +32,7 @@ class Cell {
   value: any;
   type: 'string' | 'number' | 'elapsed' | 'date' | 'duration';
   unit: string;
-  display: string
+  display: string;
 }
 
 @Component({
@@ -44,13 +44,15 @@ export class DiscoveryPageable {
   @Prop() debug = false;
   @Prop() divider: number;
   @Prop() data: Dataset;
-  @Prop({mutable: true}) options: Param = new Param();
-  @Prop({mutable: true}) params: Param[] = [];
-  @Prop({mutable: true}) elemsCount = 15;
-  @Prop({mutable: true}) windowed = 5;
+  @Prop() options: Param = new Param();
+  @Prop({ mutable: true }) params: Param[] = [];
+  @Prop({ mutable: true }) elemsCount = 15;
+  @Prop({ mutable: true }) windowed = 5;
 
   @Event() dataPointOver: EventEmitter;
   @Event() dataPointSelected: EventEmitter;
+
+  @State() innerOptions: Param;
 
   private LOG: Logger;
   private page = 0;
@@ -66,21 +68,24 @@ export class DiscoveryPageable {
     this.drawGridData();
   }
 
-  // nice hook... avoid infinite loop when globalParams return a new ElemCount
-  componentShouldUpdate(Sold, Snew, Sname) {
-    if (Sname === "options") {
-      return !Utils.deepEqual(Sold, Snew);
-    } else {
-      return true;
+  @Watch('options')
+  optionsUpdate(newValue: any, oldValue: any) {
+    this.LOG?.debug(['optionsUpdate'], newValue, oldValue);
+    let opts = newValue;
+    if (!!newValue && typeof newValue === 'string') {
+      opts = JSON.parse(newValue);
+    }
+    if (!Utils.deepEqual(opts, this.innerOptions)) {
+      this.innerOptions = { ...new Param(), ...opts };
+      this.drawGridData();
+      this.LOG?.debug(['optionsUpdate 2'], { options: this.innerOptions, newValue, oldValue });
     }
   }
 
+  // noinspection JSUnusedGlobalSymbols
   componentWillLoad() {
     this.LOG = new Logger(DiscoveryPageable, this.debug);
-    this.drawGridData();
-  }
-
-  componentWillRender() {
+    this.innerOptions = { ...new Param(), ...this.options };
     this.drawGridData();
   }
 
@@ -100,25 +105,29 @@ export class DiscoveryPageable {
   }
 
   private drawGridData() {
-    this.LOG?.debug(['drawGridData', '_options'], this.options);
+    this.LOG?.debug(['drawGridData', '_options'], this.innerOptions);
     if (!this.data) {
       return;
     }
-    const options = Utils.mergeDeep<Param>({...new Param(), timeMode: 'date'}, this.options || {});
+    const options = Utils.mergeDeep<Param>({ ...new Param(), timeMode: 'date' }, this.innerOptions || {});
     this.pages = [];
-    this.elemsCount = this.options.elemsCount || this.elemsCount;
-    this.windowed = this.options.windowed || this.windowed;
+    this.elemsCount = this.innerOptions.elemsCount || this.elemsCount;
+    this.windowed = this.innerOptions.windowed || this.windowed;
     const dataset: Cell[][] = (this.data.values || [])
       .map(row => row.map((v, i) => i === 0 ? this.formatDate(v) : this.formatValue(v)))
       .filter(d => {
-        if (Object.keys(this.filters).length === 0) { return true }
+        if (Object.keys(this.filters).length === 0) {
+          return true;
+        }
         let found = true;
-        Object.keys(this.filters).forEach(k => found = found && d[k]?.display.toLowerCase().search(this.filters[k].toLowerCase())>=0);
+        Object.keys(this.filters).forEach(k => found = found && d[k]?.display.toLowerCase().search(this.filters[k].toLowerCase()) >= 0);
         return found;
       });
     if (this.sortCol >= 0) {
       dataset.sort((a, b) => {
-        if (a[this.sortCol] == null || b[this.sortCol] == null) { return -1; }
+        if (a[this.sortCol] == null || b[this.sortCol] == null) {
+          return -1;
+        }
         switch (a[this.sortCol].type) {
           case 'string':
             return this.sortAsc
@@ -130,13 +139,13 @@ export class DiscoveryPageable {
               : b[this.sortCol].value - a[this.sortCol].value;
         }
       });
-    }    
+    }
     for (let i = 0; i < dataset.length / this.elemsCount; i++) {
       this.pages.push(i);
     }
     this.displayedValues = dataset.slice(
       Math.max(0, this.elemsCount * this.page),
-      Math.min(this.elemsCount * (this.page + 1), (this.data.values || []).length)
+      Math.min(this.elemsCount * (this.page + 1), (this.data.values || []).length),
     );
     this.LOG?.debug(['drawGridData', 'data'], this.data, {
       windowed: this.windowed,
@@ -144,7 +153,7 @@ export class DiscoveryPageable {
       displayedValues: this.displayedValues,
     });
     this.updateCounter++;
-    this.options = { ...options, extra: this.updateCounter }; // cleaner way to force a render ?
+    this.innerOptions = { ...options, extra: this.updateCounter }; // cleaner way to force a render ?
   }
 
   private static formatLabel(name: string) {
@@ -156,8 +165,8 @@ export class DiscoveryPageable {
         date: this.data.isGTS ? value[0].value : undefined,
         name: this.data.name,
         value: value,
-        meta: {header: this.data.headers}
-      }
+        meta: { header: this.data.headers },
+      },
     );
   }
 
@@ -166,20 +175,20 @@ export class DiscoveryPageable {
         date: this.data.isGTS ? value[0].value : undefined,
         name: this.data.name,
         value: value,
-        meta: {header: this.data.headers}
-      }
+        meta: { header: this.data.headers },
+      },
     );
   }
 
-  private sort(header) {
-    if (this.options?.tabular?.sortable) {
+  private sort(header: number) {
+    if (this.innerOptions?.tabular?.sortable) {
       this.sortAsc = this.sortCol !== header ? true : !this.sortAsc;
       this.sortCol = header;
       this.drawGridData();
     }
   }
 
-  private filter(i, e) {
+  private filter(i: number, e: any) {
     if (e.srcElement.value !== '') {
       this.filters[i] = e.srcElement.value;
     } else {
@@ -190,7 +199,7 @@ export class DiscoveryPageable {
 
   private formatDate(v: any): Cell {
     if (this.data.isGTS) {
-      return this.formatValue({value: v, type: 'date'});
+      return this.formatValue({ value: v, type: 'date' });
     } else {
       return this.formatValue(v);
     }
@@ -204,32 +213,32 @@ export class DiscoveryPageable {
   } {
     if (typeof v !== 'object') {
       v = {
-        value: v !== undefined? v : '',
+        value: v !== undefined ? v : '',
         type: typeof v,
         unit: '',
-        display: v.toString() || ''
+        display: v.toString() || '',
       };
     } else {
       v = {
         type: 'string',
         unit: '',
-        ...v
-      }
+        ...v,
+      };
     }
-    let res = v.value !== undefined? v.value : '';
+    let res = v.value !== undefined ? v.value : '';
     switch (v.type) {
       case 'elapsed':
         if (res !== undefined && !isNaN(parseInt(res, 10))) {
-          res = dayjs().to(dayjs(GTSLib.toISOString(parseInt(res, 10), this.divider, this.options.timeZone,
-            this.options.fullDateDisplay ? this.options.timeFormat : undefined)));
+          res = dayjs().to(dayjs(GTSLib.toISOString(parseInt(res, 10), this.divider, this.innerOptions.timeZone,
+            this.innerOptions.fullDateDisplay ? this.innerOptions.timeFormat : undefined)));
         } else {
           res = '';
         }
         break;
       case 'date':
         if (res !== undefined && !isNaN(parseInt(res, 10))) {
-          const format = v.format || (this.options.fullDateDisplay ? this.options.timeFormat : undefined);
-          res = GTSLib.toISOString(parseInt(res, 10), this.divider, this.options.timeZone, format)
+          const format = v.format || (this.innerOptions.fullDateDisplay ? this.innerOptions.timeFormat : undefined);
+          res = GTSLib.toISOString(parseInt(res, 10), this.divider, this.innerOptions.timeZone, format)
             .replace('T', ' ')
             .replace(/\+[0-9]{2}:[0-9]{2}$/gi, '');
         } else {
@@ -237,7 +246,7 @@ export class DiscoveryPageable {
         }
         break;
       case 'duration':
-        const format = v.format || (this.options.fullDateDisplay ? this.options.timeFormat : undefined);
+        const format = v.format || (this.innerOptions.fullDateDisplay ? this.innerOptions.timeFormat : undefined);
         if (res !== undefined && !isNaN(parseInt(res, 10))) {
           res = dayjs.duration(parseInt(res, 10) / this.divider).format(format);
         } else {
@@ -255,30 +264,30 @@ export class DiscoveryPageable {
 
   render() {
     return <div>
-      <div class="heading" innerHTML={DiscoveryPageable.formatLabel(this.data.name)}/>
-      {!!this.options?.tabular?.onTop ? this.getPagination() : ''}
-      <table class={!!this.options?.tabular?.stickyHeader ? "sortable nospace" : "sortable"}>
-        <thead class={!!this.options?.tabular?.stickyHeader ? "stickyHeader" : ''}>
-          <tr>
-            {this.data.headers.map((header, i) =>
+      <div class="heading" innerHTML={DiscoveryPageable.formatLabel(this.data.name)} />
+      {!!this.innerOptions?.tabular?.onTop ? this.getPagination() : ''}
+      <table class={!!this.innerOptions?.tabular?.stickyHeader ? 'sortable nospace' : 'sortable'}>
+        <thead class={!!this.innerOptions?.tabular?.stickyHeader ? 'stickyHeader' : ''}>
+        <tr>
+          {this.data.headers.map((header, i) =>
+            <th
+              data-sort={i}
+              class={this.getClasses(i)}
+              onClick={() => this.sort(i)}
+              style={{
+                width: this.innerOptions.tabular?.fixedWidth ? `${(100 / this.data.headers.length)}%` : 'auto',
+              }}>{header}</th>)
+          }
+        </tr>
+        <tr>
+          {this.innerOptions?.tabular?.filterable ? this.data.headers.map((header, i) =>
               <th
-                data-sort={i}
-                class={this.getClasses(i)}
-                onClick={() => this.sort(i)}
-                style={{
-                  width: this.options.tabular?.fixedWidth ? `${(100 / this.data.headers.length)}%` : 'auto'
-                }}>{header}</th>)
-            }
-          </tr>
-          <tr>
-            {this.options?.tabular?.filterable ? this.data.headers.map((header, i) =>
-                <th
-                  data-filter={i} style={{
-                  width: this.options.tabular?.fixedWidth ? `${(100 / this.data.headers.length)}%` : 'auto'
-                }}><input type="text" class="discovery-input" onInput={e => this.filter(i, e)}/></th>)
-              : ''
-            }
-          </tr>
+                data-filter={i} style={{
+                width: this.innerOptions.tabular?.fixedWidth ? `${(100 / this.data.headers.length)}%` : 'auto',
+              }}><input type="text" class="discovery-input" onInput={e => this.filter(i, e)} /></th>)
+            : ''
+          }
+        </tr>
         </thead>
         <tbody>
         {this.displayedValues.map((value, i) =>
@@ -286,13 +295,13 @@ export class DiscoveryPageable {
               onMouseOver={() => this.setOver(value)}
               style={this.getRowStyle(i)}
           >{value.map((v, j) =>
-            <td style={this.getCellStyle(i, j)}><span innerHTML={v.display + (v.unit || '')}/></td>
-          )}</tr>
+            <td style={this.getCellStyle(i, j)}><span innerHTML={v.display + (v.unit || '')} /></td>,
+          )}</tr>,
         )}
         </tbody>
       </table>
-      {!this.options?.tabular?.onTop ? this.getPagination() : ''}
-    </div>
+      {!this.innerOptions?.tabular?.onTop ? this.getPagination() : ''}
+    </div>;
   }
 
   private getRowStyle(row: number) {
@@ -321,10 +330,10 @@ export class DiscoveryPageable {
 
   private getClasses(i: number): string {
     const c: string[] = [];
-    if (this.options?.tabular?.sortable) {
+    if (this.innerOptions?.tabular?.sortable) {
       c.push('pointer');
     }
-    if (this.options?.tabular?.sortable && this.sortCol === i) {
+    if (this.innerOptions?.tabular?.sortable && this.sortCol === i) {
       c.push('sortable');
       if (this.sortAsc) {
         c.push('asc');
@@ -343,7 +352,7 @@ export class DiscoveryPageable {
         {this.pages.length > 1
           ? this.pages.map(c => <span>
         {c >= this.page - this.windowed && c <= this.page + this.windowed
-          ? <span class={{index: true, hoverable: this.page !== c, active: this.page === c}}
+          ? <span class={{ index: true, hoverable: this.page !== c, active: this.page === c }}
                   onClick={() => this.goto(c)}>{c}</span>
           : ''}
       </span>) : ''}
